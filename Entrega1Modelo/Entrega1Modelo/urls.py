f7191758fe4652e782da912a4033c9260cac414e
Juan Camellini <juantomascamellini@gmail.com>
2022-09-06 22:52:56 -0300

    modified:   Entrega1Modelo/Entrega1Modelo/urls.py

--- Entrega1Modelo/Entrega1Modelo/urls.py
+++ Entrega1Modelo/Entrega1Modelo/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('Aplicacion/', include('Aplicacion.urls'))
+    path('', include('Aplicacion.urls'))
 ]
