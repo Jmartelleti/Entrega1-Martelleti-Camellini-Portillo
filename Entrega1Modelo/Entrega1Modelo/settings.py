e59ae407858be738579b94e1461c12aa4dd1517f
Juan Camellini <juantomascamellini@gmail.com>
2022-09-22 14:59:51 -0300

    modified:   Entrega1Modelo/Entrega1Modelo/settings.py 	modified:   Entrega1Modelo/Aplicacion/__pycache__/admin.cpython-310.pyc 	modified:   Entrega1Modelo/Aplicacion/__pycache__/forms.cpython-310.pyc 	modified:   Entrega1Modelo/Aplicacion/__pycache__/models.cpython-310.pyc 	modified:   Entrega1Modelo/Aplicacion/__pycache__/urls.cpython-310.pyc 	modified:   Entrega1Modelo/Aplicacion/__pycache__/views.cpython-310.pyc 	modified:   Entrega1Modelo/Aplicacion/admin.py 	modified:   Entrega1Modelo/Aplicacion/forms.py 	modified:   Entrega1Modelo/Aplicacion/models.py 	modified:   Entrega1Modelo/Aplicacion/static/Aplicacion/css/login.css 	modified:   Entrega1Modelo/Aplicacion/templates/Aplicacion/about.html 	deleted:    Entrega1Modelo/Aplicacion/templates/Aplicacion/editarPerfil.html 	modified:   Entrega1Modelo/Aplicacion/templates/Aplicacion/index.html 	modified:   Entrega1Modelo/Aplicacion/templates/Aplicacion/login.html 	modified:   Entrega1Modelo/Aplicacion/urls.py 	modified:   Entrega1Modelo/Aplicacion/views.py 	modified:   Entrega1Modelo/Entrega1Modelo/__pycache__/settings.cpython-310.pyc 	modified:   Entrega1Modelo/Entrega1Modelo/__pycache__/urls.cpython-310.pyc 	modified:   Entrega1Modelo/Entrega1Modelo/urls.py 	modified:   Entrega1Modelo/db.sqlite3 	Entrega1Modelo/Aplicacion/migrations/0002_rename_usuario_usuario_username_avatar.py 	Entrega1Modelo/Aplicacion/migrations/0003_alter_avatar_img.py 	Entrega1Modelo/Aplicacion/migrations/__pycache__/0002_rename_usuario_usuario_username_avatar.cpython-310.pyc 	Entrega1Modelo/Aplicacion/migrations/__pycache__/0003_alter_avatar_img.cpython-310.pyc 	Entrega1Modelo/Aplicacion/templates/Aplicacion/editarUsuario.html 	Entrega1Modelo/Aplicacion/templates/Aplicacion/logout.html 	Entrega1Modelo/Aplicacion/templates/Aplicacion/register.html 	Entrega1Modelo/media/

--- Entrega1Modelo/Entrega1Modelo/settings.py
+++ Entrega1Modelo/Entrega1Modelo/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL ="/login"
+LOGIN_URL ="/Aplicacion/login"
 
 MEDIA_URL="/media/"
 MEDIA_ROOT= BASE_DIR / "media"
